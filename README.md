a6c0af7139c0e4e53dcfc04a9f5ed7ff3f7af3a5
Mostafa <mostafa@pop-os.localdomain>
2023-02-08 09:35:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# JavaScript Random Quote Generator (route-assignment-8)
+
+This is my work assigned by [Route-Academy](https://www.linkedin.com/company/routeacademy/mycompany/) during learning front-end web development.
+
+## Table of contents
+
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+### Screenshot
+
+![](./images/screenshot.gif)
+
+### Links
+
+- Solution URL: [Code]()
+- Live Site URL: [Live Site]())
+
+### Built with
+
+- HTML5
+- Bootstrap (simple use)
+- JS (Entry level)
+
+## Author
+
+- GitHub - [Mostafa Ragab](https://github.com/IMostafaR)
+- Linkedin - [@imostafarh](https://www.linkedin.com/in/imostafarh/)
---
+++ js/index.js
@@ -0,0 +1,63 @@
+var quoteElement = document.getElementById("quote");
+var authorElement = document.getElementById("author");
+var quoteContainer = document.getElementById("quoteContainer");
+
+var quotesReference = [
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    author: "― Oscar Wilde",
+  },
+  {
+    quote: "In the middle of difficulty lies opportunity",
+    author: "― Albert Einstein",
+  },
+  {
+    quote: "People, generally, suck.",
+    author: "― Christopher Moore",
+  },
+  {
+    quote:
+      "A question that sometimes drives me hazy: am I or are the others crazy?",
+    author: "― Albert Einstein",
+  },
+  {
+    quote:
+      "Your emotions are the slaves to your thoughts, and you are the slave to your emotions.",
+    author: "― Elizabeth Gilbert",
+  },
+  {
+    quote: "Those who have a 'why' to live, can bear with almost any 'how'.",
+    author: "― Viktor E. Frankl",
+  },
+  {
+    quote: "Maybe this world is another planet's hell.",
+    author: "― Aldous Huxley",
+  },
+  {
+    quote: "Do not take life too seriously. You will not get out alive.",
+    author: "― Elbert Hubbard",
+  },
+  {
+    quote: "The best revenge is massive success.",
+    author: "― Frank Sinatra",
+  },
+  {
+    quote:
+      "The difference between genius and stupidity is: genius has its limits.",
+    author: "― Alexandre Dumas-fils",
+  },
+];
+
+function displayItem() {
+  quoteContainer.classList.replace("invisible", "visible");
+}
+
+function refreshQuote() {
+  displayItem();
+  randomIteration =
+    quotesReference[Math.floor(Math.random() * quotesReference.length)]; //https://stackoverflow.com/questions/37167264/javascript-output-random-object-from-array-of-objects
+
+  quoteElement.innerHTML = `<i class="fa-solid fa-quote-left me-3 display-3 text-warning"></i
+  >${randomIteration.quote}<i class="fa-solid fa-quote-right ms-3 display-3 text-warning"></i>`;
+  authorElement.innerHTML = randomIteration.author;
+}
